dd6805c6b358e1f6e7fd84891d91a56912a171d4
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-27 09:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const table = document.getElementById("sampleTable");
+	const newRow = document.createElement('tr');
+	const firstCell = document.createElement('td');
+	firstCell.textContent = 'New Cell1';
+	const secondCell = document.createElement('td');
+	secondCell.textContent = 'New Cell2';
+
+	newRow.append(firstCell,secondCell);
+	table.insertBefore(newRow, table.firstChild);
 }
